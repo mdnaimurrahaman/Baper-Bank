b744dea52ed1b37ffbf2e8d040b0fd8a94823056
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-10 15:59:49 +0600

    add

--- js/Bank.js
+++ js/Bank.js
@@ -1,22 +1,19 @@
-document.getElementById('submit-btn').addEventListener
-('click', function(){
-    //ger user email
-    const emailField = document.getElementById('user-email');
-    const userEmail = emailField.value ;
-    console.log (userEmail);
+document.getElementById("submit-btn").addEventListener("click", function () {
+  //ger user email
+  const emailField = document.getElementById("user-email");
+  const userEmail = emailField.value;
+  console.log(userEmail);
 
-    //get user password
-    const passwordField = document.getElementById('user-password');
-    const userPassword = passwordField.value ;
-   
-    //check user info
-    if ( userEmail=='puska@dev.com'&& userPassword == 'emon' ){
-        console.log('valid user');
-        window.location.href='banking.html';
-    }
+  //get user password
+  const passwordField = document.getElementById("user-password");
+  const userPassword = passwordField.value;
 
-})
+  //check user info
+  if (userEmail == "sontan@baap.com" && userPassword == "secret") {
+    console.log("valid user");
+    window.location.href = "banking.html";
+  }
+});
 
 // sontan@baap.com
 // secret
-
