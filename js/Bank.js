986c15d4be968a8d10ecf0abe772c5ce8a8219fa
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-10 11:41:31 +0600

    banking page

--- js/Bank.js
+++ js/Bank.js
@@ -1,5 +1,6 @@
 document.getElementById('submit-btn').addEventListener
 ('click', function(){
+    //ger user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value ;
     console.log (userEmail);
@@ -7,8 +8,11 @@ document.getElementById('submit-btn').addEventListener
     //get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value ;
+   
+    //check user info
     if ( userEmail=='sontan@baap.com'&& userPassword == 'secret' ){
         console.log('valid user');
+        window.location.href='banking.html';
     }
 
 })
