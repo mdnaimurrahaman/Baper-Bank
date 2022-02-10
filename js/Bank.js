50c4633c642ae3b259e005c5aaee06793b34d412
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-10 16:01:43 +0600

    add

--- js/Bank.js
+++ js/Bank.js
@@ -1,19 +1,24 @@
-document.getElementById("submit-btn").addEventListener("click", function () {
-  //ger user email
-  const emailField = document.getElementById("user-email");
-  const userEmail = emailField.value;
-  console.log(userEmail);
+document.getElementById('submit-btn').addEventListener
+('click', function(){
+    //ger user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value ;
+    console.log (userEmail);
 
-  //get user password
-  const passwordField = document.getElementById("user-password");
-  const userPassword = passwordField.value;
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value ;
+   
+    //check user info
+    if ( userEmail=='sontan@baap.com'&& userPassword == 'secret' ){
+        console.log('valid user');
+        window.location.href='banking.html';
+    }
 
-  //check user info
-  if (userEmail == "sontan@baap.com" && userPassword == "secret") {
-    console.log("valid user");
-    window.location.href = "banking.html";
-  }
-});
+})
 
-// sontan@baap.com
-// secret
+//handle deposit button event
+document.getElementById('deposit-btn').addEventListener
+('click', function (){
+    console.log('button clicked')
+})
