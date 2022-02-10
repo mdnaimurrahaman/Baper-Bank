77fe23d7f2f86e8e53d256336dbc033cfc7e6f1c
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-10 15:51:51 +0600

    login page info change

--- js/Bank.js
+++ js/Bank.js
@@ -1,19 +1,16 @@
-document.getElementById('submit-btn').addEventListener
-('click', function(){
-    //ger user email
-    const emailField = document.getElementById('user-email');
-    const userEmail = emailField.value ;
-    console.log (userEmail);
+document.getElementById("submit-btn").addEventListener("click", function () {
+  //ger user email
+  const emailField = document.getElementById("user-email");
+  const userEmail = emailField.value;
+  console.log(userEmail);
 
-    //get user password
-    const passwordField = document.getElementById('user-password');
-    const userPassword = passwordField.value ;
-   
-    //check user info
-    if ( userEmail=='sontan@baap.com'&& userPassword == 'secret' ){
-        console.log('valid user');
-        window.location.href='banking.html';
-    }
+  //get user password
+  const passwordField = document.getElementById("user-password");
+  const userPassword = passwordField.value;
 
-})
-
+  //check user info
+  if (userEmail == "puskadevloper@gmail.com" && userPassword == "emon") {
+    console.log("valid user");
+    window.location.href = "banking.html";
+  }
+});
